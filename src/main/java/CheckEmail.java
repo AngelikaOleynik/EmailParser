b8415d9b15e3a1e0bc576cb08c75da40b41db421
Angelika <oleinik38@gmail.com>
2021-08-04 11:15:24 +0300

    cleanup

--- src/main/java/CheckEmail.java
+++ src/main/java/CheckEmail.java
@@ -1,11 +1,5 @@
-import org.jsoup.Jsoup;
-
 import javax.mail.*;
-import javax.mail.internet.MimeMultipart;
 import javax.mail.internet.MimeUtility;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
@@ -48,89 +42,26 @@ public class CheckEmail {
                 messagesList.add(inbox.getMessage(i));
             }
 
-            if (messagesList.get(3).isMimeType("multipart/alternative")) {
+            Message lastMessage = messagesList.get(messagesList.size());
+            if (lastMessage.isMimeType("multipart/alternative")) {
 
-                Multipart multipart = (Multipart) messagesList.get(3).getContent();
+                Multipart multipart = (Multipart) lastMessage.getContent();
 
                 for (int i = 0; i < multipart.getCount(); i++) {
                     BodyPart part = multipart.getBodyPart(i);
-                    //Для html-сообщений создается две части, "text/plain" и "text/html" (для клиентов без возможности чтения html сообщений), так что если нам не важна разметка:
+
                     if (part.isMimeType("text/plain")) {
                         System.out.println(part.getContent().toString());
                     }
-                    // Проверяем является ли part вложением
                     else if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
-                        // Опускаю проверку на совпадение имен. Имя может быть закодировано, используем decode
                         String fileName = MimeUtility.decodeText(part.getFileName());
-
-                        //InputStream is = part.getInputStream();
-                        // Далее можем записать файл, или что-угодно от нас требуется
                     }
                 }
-            } else if (messagesList.get(3).isMimeType("text/plain")) {
+            } else if (lastMessage.isMimeType("text/plain")) {
                 System.out.println(messagesList.get(3).getContent().toString());
             }
-
-            System.out.println(messagesList.get(3).getSubject() + "  === " + messagesList.get(3).getContent());
-
-            Message inboxMessages[] = inbox.getMessages();
-
-            Message firstMessage = (Message) Arrays.stream(Arrays.stream(inboxMessages).toArray()).findFirst().get();
-
-            String messageSubject = firstMessage.getSubject().replaceAll("<(.)+?>", "").trim();
-            //оставляет русский текст и символы
-            String messageContent = firstMessage.getContent().toString().replaceAll("[[\\S]&&[^А-Яа-я-.?!)(,:]]", "").trim();
-
-            System.out.println("Message subject " + messageSubject);
-            System.out.println("======================================================================================");
-            System.out.println("Message subject " + messageContent);
-            System.out.println("======================================================================================");
-
-            System.out.println("========================================================================================");
-            String content = inboxMessages[0].getAllRecipients().toString();
-            if (content.length() > 500) {
-                content = content.substring(0, 500);
-            }
-            System.out.print(content);
-
-            inbox.close(false);
-            store.close();
-
-        } catch (NoSuchProviderException noSuchProviderException) {
-            noSuchProviderException.printStackTrace();
-        } catch (MessagingException messagingException) {
-            messagingException.printStackTrace();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private String getTextFromMessage(Message message) throws MessagingException, IOException {
-        String result = "";
-        if (message.isMimeType("text/plain")) {
-            result = message.getContent().toString();
-        } else if (message.isMimeType("multipart/*")) {
-            MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
-            result = getTextFromMimeMultipart(mimeMultipart);
-        }
-        return result;
-    }
-
-    private String getTextFromMimeMultipart(MimeMultipart mimeMultipart) throws MessagingException, IOException {
-        String result = "";
-        int count = mimeMultipart.getCount();
-        for (int i = 0; i < count; i++) {
-            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
-            if (bodyPart.isMimeType("text/plain")) {
-                result = result + "\n" + bodyPart.getContent();
-                break;
-            } else if (bodyPart.isMimeType("text/html")) {
-                String html = (String) bodyPart.getContent();
-                result = result + "\n" + Jsoup.parse(html).text();
-            } else if (bodyPart.getContent() instanceof MimeMultipart) {
-                result = result + getTextFromMimeMultipart((MimeMultipart) bodyPart.getContent());
-            }
-        }
-        return result;
     }
 }
